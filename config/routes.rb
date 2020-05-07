ed17390c7e1ae1da6dd3457ff5f76b0ff029428c
fukuba0319 <mizubuka0319@gmail.com>
2020-05-07 10:04:11 +0000

    fav

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'topics/new'
   get 'sessions/new'
   root 'pages#index'
   
@@ -9,4 +8,7 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'  
+  
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'  
 end
